e73ddac9f9e4550cd8a056c92bf6f236b2c57c3c
Marcin Jackowski <>
2018-09-06 18:43:18 +0200

    Initial commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Marcin Jackowski on 06/09/2018.
+//  Copyright © 2018 CocoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
