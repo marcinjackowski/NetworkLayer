45e567782f89e58513b07107b4658e9fd38a7603
Marcin Jackowski <>
2018-09-06 18:48:59 +0200

    add urlComponentsextension

---
+++ NetworkLayer/Extensions/URLComponentsExtension.swift
@@ -0,0 +1,23 @@
+//
+//  URLComponentsExtension.swift
+//  NetworkLayer
+//
+//  Created by Marcin Jackowski on 06/09/2018.
+//  Copyright © 2018 CocoApps. All rights reserved.
+//
+
+import Foundation
+
+extension URLComponents {
+    
+    init(service: ServiceProtocol) {
+        let url = service.baseURL.appendingPathComponent(service.path)
+        self.init(url: url, resolvingAgainstBaseURL: false)!
+        
+        guard case let .requestParameters(parameters) = service.task, service.parametersEncoding == .url else { return }
+        
+        queryItems = parameters.map { key, value in
+            return URLQueryItem(name: key, value: String(describing: value))
+        }
+    }
+}
